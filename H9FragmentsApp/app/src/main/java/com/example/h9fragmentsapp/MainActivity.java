3fa05fbdc2d0a883a80255695549dccb340722fc
Tej <reddytej15@gmail.com>
2025-02-23 18:03:49 +0530

    add:fragments app

--- H9FragmentsApp/app/src/main/java/com/example/h9fragmentsapp/MainActivity.java
+++ H9FragmentsApp/app/src/main/java/com/example/h9fragmentsapp/MainActivity.java
@@ -1,24 +1,73 @@
 package com.example.h9fragmentsapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fm;
+    FragmentTransaction ft;
+    SportsFragment sf;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_activity_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        try {
+            sf = new SportsFragment();
+            fm = getSupportFragmentManager();
+
+            fm.setFragmentResultListener("requestKey", this, new FragmentResultListener() {
+                @Override
+                public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                    ft = fm.beginTransaction();
+                    int option = result.getInt("SelectedItemIndex");
+
+                    switch (option) {
+                        case 0:
+                            ft.replace(R.id.ll_bottom, new BadmintonFragment());
+                            break;
+                        case 1:
+                            ft.replace(R.id.ll_bottom, new CricketFragment());
+                            break;
+                        case 2:
+                            ft.replace(R.id.ll_bottom, new TennisFragment());
+                            break;
+                        case 3:
+                            ft.replace(R.id.ll_bottom, new HockeyFragment());
+                            break;
+                        default:
+                            ft.replace(R.id.ll_bottom, new HockeyFragment());
+                    }
+                    ft.commit();
+                }
+            });
+            ft = fm.beginTransaction();
+            ft.add(R.id.ll_top, sf);
+            ft.commit();
+        }
+        catch (Exception ex) {
+            Log.d("INITIAL ERROR:->", "onCreate: " + ex.getMessage());
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- H9FragmentsApp/app/src/main/java/com/example/h9fragmentsapp/SportsFragment.java
+++ H9FragmentsApp/app/src/main/java/com/example/h9fragmentsapp/SportsFragment.java
@@ -2,63 +2,48 @@ package com.example.h9fragmentsapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SportsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SportsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class SportsFragment extends Fragment implements AdapterView.OnItemClickListener {
+    ListView lv;
 
     public SportsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SportsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SportsFragment newInstance(String param1, String param2) {
-        SportsFragment fragment = new SportsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sports, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        lv = view.findViewById(R.id.lv_fragment);
+        lv.setOnItemClickListener(this);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        try {
+            Toast.makeText(getContext(), lv.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            Bundle bun = new Bundle();
+            bun.putInt("SelectedItemIndex", i);
+            getParentFragmentManager().setFragmentResult("requestKey", bun);
+        } catch (Exception ex) {
+            Log.d("EXCEPTION", "onItemClick: " + ex.getMessage());
+            Toast.makeText(getContext(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
